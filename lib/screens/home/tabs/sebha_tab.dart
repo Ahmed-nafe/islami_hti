1ab3288da85a368cbb0f85d8e00d717d9df3f70e
AboNaf3 <abonafe27@gmail.com>
2023-09-10 20:05:38 +0300

    add some photo

--- lib/screens/home/tabs/sebha_tab.dart
+++ lib/screens/home/tabs/sebha_tab.dart
@@ -5,6 +5,19 @@ class SebhaTab extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.green,);
+    double h =MediaQuery.of(context).size.height;
+    double w =MediaQuery.of(context).size.width;
+
+    return Container(
+      height: h*0.5,
+      width: double.infinity,
+      child: Column(
+        children: [
+          Image.asset('assets/ic_sebha.png' ,color: Colors.red,),
+
+
+        ],
+      ),
+    );
   }
 }
